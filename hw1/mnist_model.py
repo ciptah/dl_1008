65b46196f169a993f28df8a2a42181f3181e9692
cipta <ciphwn@gmail.com>
2017-02-12 15:37:39 -0500

    blah

--- hw1/mnist_model.py
+++ hw1/mnist_model.py
@@ -2,13 +2,24 @@ import sys
 import json
 import logging
 
+logger = logging.getLogger('main')
+
 if len(sys.argv) < 2:
-    logging.warn('Need a config filename!')
+    logger.warn('Need a config filename!')
     sys.exit(1)
 
+if len(sys.argv) >= 3 and sys.argv[2] == 'debug':
+    logging.basicConfig(level=logging.DEBUG)
+
 config_filename = sys.argv[1]
-logging.info('Loading JSON config from %s', config_filename)
+logger.info('Loading JSON config from %s', config_filename)
 with open(config_filename, 'r') as f:
     config_json = json.load(f)
 
-logging.debug('Config: %s', json.dumps(config_json))
+logger.debug('CONFIGURATION: %s', json.dumps(config_json, indent=2))
+
+# Parameters are loaded, now use them to read the data.
+
+# Do some training.
+
+# Output the model file.
