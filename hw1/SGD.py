936416d103a689f30cd69f6906cf4af21d2fcc89
WeitaoLin <lin@172-16-24-246.DYNAPOOL.NYU.EDU>
2017-02-12 15:56:27 -0500

    add custom SGD.py

---
+++ hw1/SGD.py
@@ -0,0 +1,11 @@
+"""
+Module that do stochastic gradient descent using momentum method and Nesterov's Accelerated Gradient method 
+"""
+
+
+class SGD:
+    """
+    Class that implements sgd
+    """
+    def __init__(self):
+        return None
