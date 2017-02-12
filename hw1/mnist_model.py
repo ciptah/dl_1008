30c8fe28d468abcd8b79070adaadc5bc973fc43a
cipta <ciphwn@gmail.com>
2017-02-12 16:14:23 -0500

    training parameters

--- hw1/mnist_model.py
+++ hw1/mnist_model.py
@@ -12,18 +12,18 @@ config_filename = sys.argv[1]
 with open(config_filename, 'r') as f:
     config_json = json.load(f)
 
-if 'verbose' in config_json and config_json['verbose']:
+logger.info('Loaded JSON config from %s', config_filename)
+
+if config_json.get('verbose', False):
     logging.basicConfig(level=logging.DEBUG)
     logger.debug('Turning on verbose logging.')
 else:
     # Default log level is info.
     logging.basicConfig(level=logging.INFO)
 
-logger.info('Loaded JSON config from %s', config_filename)
 logger.debug('CONFIGURATION: %s', json.dumps(config_json, indent=2))
 
 # Parameters are loaded, now use them to read the data.
-
 # Do some training.
 
 # Output the model file.
