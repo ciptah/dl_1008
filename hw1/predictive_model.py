89a6ff958bf8bc8a0d8875cbca5005a0f958161d
Cipta Herwana <ciphwn@gmail.com>
2017-02-22 10:53:08 -0500

    Make 'model' and # epochs configurable, and other minor changes. (#6)
    * origin
    
    * comment
    
    * consta
    
    * refactor
    
    * with open syntax
    
    * rm
    
    * preluss

--- hw1/predictive_model.py
+++ hw1/predictive_model.py
@@ -2,45 +2,12 @@
 Module that contains all code for the MNIST predictive model
 """
 import torch.nn.functional as F
-import torch.nn as nn
 import torch.optim as optim
 
+import constants
 import logging
 
 logger = logging.getLogger('predictive_model')
-
-class Net(nn.Module):
-    """
-    Sample Model
-    """
-    def __init__(self, dropout=0.5):
-        super(Net, self).__init__()
-        self.dropout = dropout
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d(p=self.dropout)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        # Input is 1x28x28.
-        # N (minibatch size) is implicit.
-
-        # Conv1: 1x28x28 -> 10x24x24
-        # Pool: 10x24x24 -> 10x12x12
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        # Conv2: 10x12x12 -> 20x8x8
-        # Pool: 20x8x8 -> 20x4x4
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        # View: 20x4x4 -> 320
-        x = x.view(-1, 320)
-        # FC: 320 -> 50
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        # FC: 50->10
-        x = F.relu(self.fc2(x))
-        return F.log_softmax(x)
-
 
 class PredictiveModel:
     """
@@ -48,12 +15,15 @@ class PredictiveModel:
     """
     def __init__(self, config):
         learning_rate = config['training']['learning_rate']
-        dropout = config['training']['dropout']
         momentum = config['training']['momentum']['mu_coefficient']
 
         self.config = config
-        self.model = Net(dropout=dropout)
 
+        model = config.get('model', 'basic')
+        logger.info('Using model "%s".', model)
+        self.model = constants.models[model](config)
+
+        # TODO: optimization method should probably be configurable too.
         self.optimizer = optim.SGD(self.model.parameters(),
                 lr=learning_rate, momentum=momentum)
         self.loss_func = F.nll_loss
