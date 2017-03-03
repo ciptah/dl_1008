6131556fef3edee4a8b9d073f2381445481ec3e0
seyiqi <seyiqi@gmail.com>
2017-03-03 15:56:11 -0500

    SWWAE finished. Added function that produces a df for prediction.

--- hw1/mnist_model.py
+++ hw1/mnist_model.py
@@ -5,9 +5,12 @@ import predictive_model
 import data_provider
 from torch.autograd import Variable
 import pickle
+import numpy as np
+import pandas as pd
+import os
 
 
-def train_epoch(model, train_data_provider, current_epoch, report_period=10):
+def train_epoch(model, train_data_provider, current_epoch, report_period=100, unlablled=False):
     """
     Function that trains a model for one epoch
     :param model:
@@ -17,13 +20,21 @@ def train_epoch(model, train_data_provider, current_epoch, report_period=10):
     :return:
     """
     train_loader = train_data_provider.loader
+    total_num = 0
+    correct_num = 0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = Variable(data), Variable(target)
-        pred, loss = model.train_batch(data, target)
+        pred_dist, loss = model.train_batch(data, target)
+        if not unlablled:
+            pred = pred_dist[0].data.max(1)[1]
+            correct_num += (pred == target.data).sum()
+            total_num += len(pred)
         if batch_idx % report_period == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 current_epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss))
+                               100. * batch_idx / len(train_loader), loss))
+    if not unlablled:
+        print("Train Epoch {0}, train_acc = {1}".format(current_epoch, float(correct_num) / total_num))
 
 
 def validate_model(model, test_data_provider):
@@ -39,13 +50,27 @@ def validate_model(model, test_data_provider):
     correct = 0
     for data, target in test_loader:
         data, target = Variable(data), Variable(target)
-        output, pred, loss_val = model.validate_batch(data, target)
+        _, pred, loss_val = model.validate_batch(data, target)
         test_loss += loss_val
         correct += pred.eq(target.data).cpu().sum()
-    test_loss /= len(test_loader) # loss function already averages over batch size
+    test_loss /= len(test_loader)  # loss function already averages over batch size
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    return float(correct) / len(test_loader.dataset)
+
+
+def predict_model(model, pred_data_loader):
+    model.start_prediction()
+    pred_loader = pred_data_loader.loader
+    label_predict = []
+    for _, content in enumerate(pred_loader):
+        data = Variable(content[0])
+        output = model.predict_batch(data)
+        pred = output[1].numpy().tolist()
+        label_predict += [x[0] for x in pred]
+    df = pd.DataFrame({"ID": range(len(label_predict)), "label": label_predict})
+    return df
 
 
 def build_config(config_filename, logger):
@@ -82,21 +107,44 @@ if __name__ == "__main__":
     config_filename = sys.argv[1]
 
     # parse config
-    config = build_config(config_filename, logger)
+    config = build_config("sample_config.json", logger)
 
     # load data
+    # comment out loader if you don't need. they take a hell lot of time!
     train_provider = data_provider.DataProvider(file_dir="train_labeled.p", train=True)
     logger.info('train provider loaded')
+    train_unlabeled = data_provider.DataProvider(file_dir="train_unlabeled.p", train=True)
+    logger.info('train unlabelled provider loaded')
     validation_provider = data_provider.DataProvider(file_dir="validation_data.p", train=False)
     logger.info('validation provider loaded')
+    pred_label = data_provider.DataProvider(file_dir="test.p", train=False)
+    logger.info('test provider loaded')
 
     # start training
     model = predictive_model.PredictiveModel(config)
-    for epoch in range(1, 11):
-        model.start_train()
-        train_epoch(model, train_provider, epoch)
-        model.start_prediction()
-        validate_model(model, validation_provider)
+    save_dir = "./data/model/"
+    save_name = "SWWAE"
+    try:
+        best_acc = 0
+        for epoch in range(1, 2000):
+            model.start_train()
+            train_epoch(model, train_provider, epoch)
+            train_epoch(model, train_unlabeled, epoch, unlablled=True)
+            model.start_prediction()
+            acc = validate_model(model, validation_provider)
+            print("Epoch {0}, validation acc = {1}".format(epoch, acc))
+            if acc > best_acc:
+                pickle.dump(model, open(os.path.join(save_dir, "{0}_best.p".format(save_name)), "wb"))
+                best_acc = acc
+                logger.info('best model found at acc = {0}, best model saved to {1}'.format(acc, os.path.join(save_dir,
+                                                                                                              "{0}_best.p".format(
+                                                                                                                  save_name))))
+            else:
+                pickle.dump(model, open(os.path.join(save_dir, "{0}_latest.p".format(save_name)), "wb"))
+                logger.info('model saved to {0}'.format(os.path.join(save_dir, "{0}_latest.p".format(save_name))))
+    finally:
+        pickle.dump(model, open(os.path.join(save_dir, "{0}_latest.p".format(save_name)), "wb"))
+        logger.info('model saved to {0}'.format(os.path.join(save_dir, "{0}_latest.p".format(save_name))))
 
     # save model
     pickle.dump(model, open("sample_save_model.m", "wb"))
--- hw1/predictive_model.py
+++ hw1/predictive_model.py
@@ -4,6 +4,8 @@ Module that contains all code for the predictive model
 import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
+from torch.autograd import Variable
+
 
 
 class Net(nn.Module):
@@ -19,13 +21,134 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        # x: 64 * 1 * 28 * 28
+        x = self.conv1(x)
+        # after conv: 64*10*24*24
+        x = F.max_pool2d(x, 2)
+        # after pool 64 * 10 * 12 * 12
+        x = F.relu(x)
+        # after relu: 64*10*12*12
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
         return F.log_softmax(x)
+
+    def loss_func(self, output, target):
+        return [F.nll_loss(output, target)]
+
+
+class SWWAE(nn.Module):
+    """
+    Stacked What-Where Auto-Encoders
+    """
+    def __init__(self, lambda_rec=0.41, lambda_m=0.41):
+        super(SWWAE, self).__init__()
+        # init var
+        self.lambda_rec = lambda_rec
+        self.lambda_m = lambda_m
+        self.conv1_kernel_size = 10
+        self.conv2_kernel_size = 20
+
+        # encoder
+        self.encoder_conv1 = nn.Conv2d(1, self.conv1_kernel_size, kernel_size=5)
+        self.encoder_conv2 = nn.Conv2d(self.conv1_kernel_size, self.conv2_kernel_size, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.encoder_fc1 = nn.Linear(self.conv2_kernel_size*4*4, 50)
+        self.encoder_fc2 = nn.Linear(50, 10)
+        # decoder
+        self.decoder_conv1 = nn.ConvTranspose2d(self.conv1_kernel_size, 1, kernel_size=5)
+        self.decoder_conv2 = nn.ConvTranspose2d(self.conv2_kernel_size, self.conv1_kernel_size, kernel_size=5)
+        self.decoder_unpool2 = nn.MaxUnpool2d(2)
+        self.decoder_unpool1 = nn.MaxUnpool2d(2)
+
+    def forward(self, x):
+        # encoder: x = 28 * 28
+        x_val = F.relu(self.encoder_conv1(x)) # 24 * 24
+        x_encode_m1, encode_where_1 = F.max_pool2d(x_val, 2, return_indices=True) # 12 * 12
+        x_val = F.relu(self.encoder_conv2(x_encode_m1)) # 8 * 8
+        x_val = self.conv2_drop(x_val)
+        x_encode_m2, encode_where_2 = F.max_pool2d(x_val, 2, return_indices=True) # 4 * 4
+        # prediction
+        x_out = x_encode_m2.view(-1, self.conv2_kernel_size*4*4)
+        x_out = F.relu(self.encoder_fc1(x_out))
+        x_out = F.dropout(x_out, training=self.training)
+        x_out = F.relu(self.encoder_fc2(x_out))
+        x_out = F.log_softmax(x_out)
+
+        # decoder
+        x_val = self.decoder_unpool2(x_encode_m2, encode_where_2) # 8 * 8
+        x_decode_m1 = F.relu(self.decoder_conv2(x_val)) # 12 * 12
+        x_val = self.decoder_unpool1(x_decode_m1, encode_where_1) # 24 * 24
+        x_rec = self.decoder_conv1(x_val) # 28 * 28
+        return x_out, x_encode_m1, x_decode_m1, x_rec, x
+
+
+    def loss_func(self, output, target):
+        x_out, x_encode_m1, x_decode_m1, x_rec, x = output
+        n = x.size()[0]
+        # unsupervised samples should have -1 label
+        # pseudo-label
+        # if -1 in target.data:
+        #     max_class = x_out.data.max(1)[1]
+        #     max_class = max_class.view(-1)
+        #     target = Variable(max_class)
+        # loss_nll = F.nll_loss(x_out, target)
+        loss_nll = F.nll_loss(x_out, target) if -1 not in target.data else 0
+        # L2m
+        sq_diff_m1 = (x_encode_m1-x_decode_m1)**2
+        sq_diff_m1 = sq_diff_m1.view(n,-1)
+        m1_dim = sq_diff_m1.size()[1]
+        loss_m1 = sq_diff_m1.sum(1).sum() / n / m1_dim
+        # L2rec
+        sq_diff_rec = ((x-x_rec)**2)
+        sq_diff_rec = sq_diff_rec.view(n,-1)
+        rec_dim = sq_diff_rec.size()[1]
+        loss_rec = sq_diff_rec.sum(1).sum() / n / rec_dim
+        return loss_nll + self.lambda_m * loss_m1 + self.lambda_rec * loss_rec
+
+
+class EnsembleModel:
+    """
+    Class that represents a ensemble of predictive model
+    """
+    def __int__(self, models):
+        self.models = models
+
+    def start_train(self):
+        for model in self.models:
+            model.train()
+
+    def start_prediction(self):
+        for model in self.models:
+            model.eval()
+
+    def validate_batch(self, data, target):
+        """
+        Method that validates a batch of data
+        :param data:
+        :param target:
+        :return:
+        """
+        #for model in self.models:
+        #    output, _ = model.predict_batch(data)
+        #output = output.mean()
+
+        #return output, pred, None
+
+    def predict_batch(self, data):
+        """
+        Method that predicts a batch of data
+        :param data:
+        :return:
+        """
+        output_list = []
+        for model in self.models:
+            output = model(data)
+            output_list.append(output)
+        pred = output[0].data.max(1)[1]  # get the index of the max log-probability
+        return output, pred
 
 
 class PredictiveModel:
@@ -34,9 +157,9 @@ class PredictiveModel:
     """
     def __init__(self, config):
         self.config = config
-        self.model = Net()
+        #self.model = Net()
+        self.model = SWWAE()
         self.optimizer = optim.SGD(self.model.parameters(), lr=0.01, momentum=0.05)
-        self.loss_func = F.nll_loss
 
     def start_train(self):
         self.model.train()
@@ -52,9 +175,13 @@ class PredictiveModel:
         :param kwargs:
         :return:
         """
+        # data: 64 * 1 * 28 * 28
+        # target: 64
         self.optimizer.zero_grad()
         output = self.model(data)
-        loss = self.loss_func(output, target)
+        # output: 64 * 10
+        loss = self.model.loss_func(output, target)
+        # loss:  (1,)
         loss.backward()
         self.optimizer.step()
         return output, loss.data[0]
@@ -67,7 +194,7 @@ class PredictiveModel:
         :return:
         """
         output, pred = self.predict_batch(data)
-        loss_val = self.loss_func(output, target).data[0]
+        loss_val = self.model.loss_func(output, target).data[0]
         return output, pred, loss_val
 
     def predict_batch(self, data):
@@ -77,7 +204,7 @@ class PredictiveModel:
         :return:
         """
         output = self.model(data)
-        pred = output.data.max(1)[1]  # get the index of the max log-probability
+        pred = output[0].data.max(1)[1]  # get the index of the max log-probability
         return output, pred
 
 
