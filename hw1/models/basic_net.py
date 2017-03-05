35c3cb1108d7169cd57434e59797bfbb625103c9
seyiqi <seyiqi@gmail.com>
2017-03-05 16:02:09 -0500

    merge head

--- hw1/models/basic_net.py
+++ hw1/models/basic_net.py
@@ -3,83 +3,59 @@ Module that contains the basic neural net model.
 """
 import torch.nn.functional as F
 import torch.nn as nn
-import torch.optim as optim
+from torch.autograd import Variable
+import constants
 
 import logging
 
 logger = logging.getLogger('basic_net')
 
+
 class Net(nn.Module):
     """
-    Sample Model.
-
-    This is the model from the example code, adapted a bit so the momentum
-    and dropout parameters can be tuned via configuration parameters.
-
-    It's a ConvNet with 2 convolution and 2 fully connected layers.
-
-    Worth noting that the nn.Conv2d and nn.Linear modules provided by PyTorch
-    use the scaled initialization standard deviation found in He et al.
+    Sample Model
     """
-    def __init__(self, dropout=0.5, prelu=False):
+    def __init__(self, psuedo_label_alpha_func=None):
         super(Net, self).__init__()
-        self.dropout = dropout
-
-        # TODO: Configurable sizes/channels/parameters.
-        conv1_channels = 10
-        conv2_channels = 20
-
-        self.conv1 = nn.Conv2d(1, conv1_channels, kernel_size=5)
-        self.conv2 = nn.Conv2d(conv1_channels, conv2_channels, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d(p=self.dropout)
-
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
-
-        self.prelu = prelu
-        if prelu:
-            logger.info('using PReLU.')
-            self.conv1_prelu = nn.PReLU(10)
-            self.conv2_prelu = nn.PReLU(20)
-            self.fc1_prelu = nn.PReLU(50)
-            self.fc2_prelu = nn.PReLU(10)
-        else:
-            self.register_parameter('conv1_prelu', None)
-            self.register_parameter('conv2_prelu', None)
-            self.register_parameter('fc1_prelu', None)
-            self.register_parameter('fc2_prelu', None)
-
-        logger.info('dropout = %f', dropout)
-        logger.info('%d channels for 1st convnet', conv1_channels)
-        logger.info('%d channels for 2nd convnet', conv2_channels)
-
-    def relu(self, input, which_prelu):
-        """Pick which rectifier unit to use, ReLU or PReLU."""
-        if self.prelu:
-            return which_prelu(input)
-        else:
-            return F.relu(input)
+        self.psuedo_label_alpha_func = psuedo_label_alpha_func
+        self.current_epoch_num = 1
 
     def forward(self, x):
-        # Input is 1x28x28.
-        # N (minibatch size) is implicit.
-
-        # Conv1: 1x28x28 -> 10x24x24
-        # Pool: 10x24x24 -> 10x12x12
-        x = self.relu(F.max_pool2d(self.conv1(x), 2), self.conv1_prelu)
-        # Conv2: 10x12x12 -> 20x8x8
-        # Pool: 20x8x8 -> 20x4x4
-        x = self.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2), self.conv2_prelu)
-        # View: 20x4x4 -> 320
+        # x: 64 * 1 * 28 * 28
+        x = self.conv1(x)
+        # after conv: 64*10*24*24
+        x = F.max_pool2d(x, 2)
+        # after pool 64 * 10 * 12 * 12
+        x = F.relu(x)
+        # after relu: 64*10*12*12
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
-        # FC: 320 -> 50
-        x = self.relu(self.fc1(x), self.fc1_prelu)
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        # FC: 50->10
-        x = self.relu(self.fc2(x), self.fc2_prelu)
-        return F.log_softmax(x)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = F.relu(self.fc2(x))
+        return [F.log_softmax(x)]
+
+    def loss_func(self, output, target):
+        unlabel_epoch = (-1 in target.data)
+        if unlabel_epoch:
+            if self.psuedo_label_alpha_func is not None:
+                max_class = output[0].data.max(1)[1]
+                max_class = max_class.view(-1)
+                target = Variable(max_class)
+            loss_nll = F.nll_loss(output[0], target)
+        else:
+            loss_nll = F.nll_loss(output[0], target)
+        if self.psuedo_label_alpha_func is not None:
+            loss_nll *= self.psuedo_label_alpha_func(unlabel_epoch, self.current_epoch_num)
+        return loss_nll
+
 
 def basicNet(config):
-    dropout = config.get('training', {}).get('dropout', '0.5')
-    prelu = config.get('training', {}).get('PReLU', False)
-    return Net(dropout=dropout, prelu=prelu)
+    pseudo_func_key = config.get('pseudo_label_func', None)
+    pseudo_func = None if pseudo_func_key is None else constants.psuedo_label_func_dict[pseudo_func_key]
+    return Net(psuedo_label_alpha_func=pseudo_func)
