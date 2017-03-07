a6f729a3af01fd694e6e68cafb8b149cab0514f0
cipta <ciphwn@gmail.com>
2017-03-07 09:34:04 -0500

    dropout

--- hw1/models/improved_net.py
+++ hw1/models/improved_net.py
@@ -21,12 +21,9 @@ class ImprovedNet(nn.Module):
         self.conv1a = nn.Conv2d(10, 20, kernel_size=5) # 24 -> 20
         self.conv1b = nn.Conv2d(20, 30, kernel_size=5) # 20 -> 16
         self.conv1c = nn.Conv2d(30, 60, kernel_size=5) # 16 -> 12
-        self.conv1_drop = nn.Dropout2d(p=self.dropout)
 
         self.conv2 = nn.Conv2d(60, 90, kernel_size=3)  # 12 -> 10
-        self.conv2_drop = nn.Dropout2d(p=self.dropout)
         self.conv3 = nn.Conv2d(90, 100, kernel_size=3) # 10 -> 8 -> 4
-        self.conv3_drop = nn.Dropout2d(p=self.dropout)
         self.fc1 = nn.Linear(1600, 800)
         self.fc2 = nn.Linear(800, 10)
         self.psuedo_label_alpha_func = psuedo_label_alpha_func
@@ -41,14 +38,13 @@ class ImprovedNet(nn.Module):
         x = F.relu(x)
         x = self.conv1c(x)
         x = F.relu(x)
-        x = self.conv1_drop(x)
         # after relu: 64*10*12*12
 
         #x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = F.relu(self.conv2(x))
         # after second conv pool 64 * 20 * 10 * 10
 
-        x = F.relu(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x)), 2)
         # after third conv pool 64 * 20 * 4 * 4
 
         x = x.view(-1, 1600)
