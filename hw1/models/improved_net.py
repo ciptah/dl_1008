ed145b4a8ed4e1c7f0ef93f326f195971be07f56
cipta <ciphwn@gmail.com>
2017-03-06 23:40:05 -0500

    improve models

--- hw1/models/improved_net.py
+++ hw1/models/improved_net.py
@@ -18,15 +18,14 @@ class ImprovedNet(nn.Module):
         super(ImprovedNet, self).__init__()
         self.dropout = 0.3
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.prelu1 = nn.PReLU(10*12*12)
+        self.prelu1 = nn.PReLU()
         self.conv2 = nn.Conv2d(10, 20, kernel_size=3)
         self.conv2_drop = nn.Dropout2d(p=self.dropout)
-        self.prelu2 = nn.PReLU(20*10*10)
-        self.conv3 = nn.Conv2d(20, 40, kernel_size=3)
+        self.prelu2 = nn.PReLU()
+        self.conv3 = nn.Conv2d(20, 20, kernel_size=3)
         self.conv3_drop = nn.Dropout2d(p=self.dropout)
-        self.prelu3 = nn.PReLU(40*4*4)
-        self.fc1 = nn.Linear(640, 100)
-        self.prelu4 = nn.PReLU(100)
+        self.prelu3 = nn.PReLU()
+        self.fc1 = nn.Linear(320, 100)
         self.fc2 = nn.Linear(100, 10)
         self.psuedo_label_alpha_func = psuedo_label_alpha_func
         self.current_epoch_num = 1
@@ -44,8 +43,8 @@ class ImprovedNet(nn.Module):
         x = self.prelu3(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2))
         # after third conv pool 64 * 20 * 4 * 4
 
-        x = x.view(-1, 640)
-        x = self.prelu4(self.fc1(x))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training, p=self.dropout)
         x = self.fc2(x)
         return [F.log_softmax(x)]
